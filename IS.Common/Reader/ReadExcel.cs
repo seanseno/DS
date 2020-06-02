1dc18e7490caeca412cf9f2f1ba00e96aab4fc22
seanseno <seanseno@gmail.com>
2020-06-02 12:54:09 +0800

    no message

---
+++ IS.Common/Reader/ReadExcel.cs
@@ -0,0 +1,29 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS.Common.Reader
+{
+    public static class ReadExcel
+    {
+        public static DataTableCollection Read(string sFile)
+        {
+            using (var stream = File.Open(sFile, FileMode.Open, FileAccess.Read))
+            {
+                using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                    });
+                    return result.Tables;
+                }
+            }
+        }
+    }
+}
---
+++ IS.Common/Utilities/DateTimeConvertion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS.Common.Utilities
+{
+    public static class DateTimeConvertion
+    {
+        public static string ConvertDateString(DateTime entity)
+        {
+            //return entity.ToString("yyyy-MM-dd HH:mm:ss");
+            return entity.ToString("yyy-MM-dd HH:mm:ss");
+        }
+    }
+}
